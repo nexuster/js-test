1feb28a9d2023eba57089056a71e675e5836acac
nexuster <coralpinklmao@outlook.com>
2025-02-06 21:21:53 -0500

    hi

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function loadSite() {
     var siteToLoad = document.getElementById('tests').value;
-    alert('you will be sent to document "'+siteToLoad+'."')
+    alert('you will be sent to document "' + siteToLoad + '."');
     if (siteToLoad == 'messages.html') {
-        alert('notice: this is not an online service')
+        alert('notice: this is not an online service');
     }
     window.location.href = "tests/" + siteToLoad;
 }
 
-//LONELY MESSAGES
+// LONELY MESSAGES
 
 function send() {
     var para = document.createElement('p');
@@ -21,26 +21,22 @@ function send() {
     }
 }
 
-//URL PARAMS
+// URL PARAMS
 
 window.onload = function() {
     var upara = new URLSearchParams(window.location.search);
-    if (window.location.search == 'https://nexuster.github.io/js-test/tests/upara/output.html?text='+upara.get('text')) {
-        var output = document.getElementById('output');
-
+    var output = document.getElementById('output');
+    if (output && upara.get('text')) {
         output.textContent = upara.get('text');
     }
-        
 }
 
 function sendOutput() {
     var text = document.getElementById('upara-text').value;
-    if (text) {
-        alert('"'+text+' will now be sent');
+    if (text && text.trim() !== '') {
+        alert('"' + text + '" will now be sent');
         window.location.href = "output.html?text=" + encodeURIComponent(text);
     } else {
-        if (error || text.trim() == '') {
-            alert('something prevented the code from running correctly');
-        }
+        alert('something prevented the code from running correctly');
     }
 }
