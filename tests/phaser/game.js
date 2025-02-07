b4efe440e6261770f66ddb5d51c87e7f0979c102
nexuster <coralpinklmao@outlook.com>
2025-02-07 12:21:36 -0500

    im witowee about to crash our

--- tests/phaser/game.js
+++ tests/phaser/game.js
@@ -13,13 +13,15 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload () {
+    console.log('Preloading assets...');
     this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
 }
 
 function create () {
+    console.log('Creating scene...');
     this.add.image(400, 300, 'sky');
 }
 
 function update () {
-    
+    console.log('Updating scene...');
 }
