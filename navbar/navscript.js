d9c0d26b09351f139b92216f0f2a07dd7c9dceb8
nexuster <coralpinklmao@outlook.com>
2025-02-07 08:28:33 -0500

    bru

--- navbar/navscript.js
+++ navbar/navscript.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-    if (!window.location.href == 'navbar/navbar.html') {
-        alert('test for href check')
-        var navSpan = document.getElementById('navbar');
-        var navDiv = document.createElement('div');
+    alert('test for active js')
+    var navSpan = document.getElementById('navbar');
+    var navDiv = document.createElement('div');
 
-        navDiv.appendChild(navSpan);
-    }
+    navDiv.appendChild(navSpan);
 }
