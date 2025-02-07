2ed200910dd51bbadb7bb37b9880d95003c1fd84
nexuster <coralpinklmao@outlook.com>
2025-02-07 11:55:01 -0500

    phaser add and navscript fix

---
+++ tests/phaser/phaser.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+}
+
+function create () {
+    this.add.image(400, 300, 'sky');
+}
+
+function update () {
+    
+}
